70f17aa448f1cc6631b5e949a64513bdfe8c4955
LucasYohan <lucasyohanfmarinho@gmail.com>
2025-02-13 22:31:29 -0300

    Add useState

--- pw3-somativa-2025-1/src/App.jsx
+++ pw3-somativa-2025-1/src/App.jsx
@@ -1,27 +1,40 @@
 import "./App.css";
+import { useState } from "react";
 import BookCard from "./components/BookCard";
 import GuerraCivil from "./assets/imgGuerraCivil.jpg";
 import DiarioDeUmBanana from "./assets/imgDiarioDeUmBanana.jpg";
 
 function App() {
+  const [livro, setLivro] = useState(0);
+
+  function handleChange(e) {
+    setLivro(e.target.value);
+  }
+
   return (
     <>
       <div>
         <h1>PW3 - WEBAPP - LIVRARIA</h1>
-        <BookCard
-          titulo="Um diario de um banana"
-          autor="Jeff Kinney"
-          imagem={DiarioDeUmBanana}
-        />
+        {livro ? (
+          <BookCard
+            titulo="Um diario de um banana"
+            autor="Jeff Kinney"
+            imagem={DiarioDeUmBanana}
+          />
+        ) : (
+          <BookCard
+            titulo="Vigadores: Guerra Civil" 
+            autor="Mark Millar"
+            imagem={GuerraCivil}
+          />
+        )}
 
-        <BookCard
-          titulo="Vigadores: Guerra Civil"
-          autor="Mark Millar"
-          imagem={GuerraCivil}
-        />
+        <button type="button" onClick={() => setLivro(!livro)}>
+          Teste
+        </button>
       </div>
     </>
   );
 }
 
-export default App;
+export default App; 
