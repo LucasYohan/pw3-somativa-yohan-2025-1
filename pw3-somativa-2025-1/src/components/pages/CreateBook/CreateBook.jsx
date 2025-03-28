a1f30a4540abfd11852b19bbb5d6b239bbfd41ec
LucasYohan <lucasyohanfmarinho@gmail.com>
2025-03-27 21:10:52 -0300

    Correcao form

--- pw3-somativa-2025-1/src/components/pages/CreateBook/CreateBook.jsx
+++ pw3-somativa-2025-1/src/components/pages/CreateBook/CreateBook.jsx
@@ -1,36 +1,72 @@
 import Input from "../../Form/Input";
 import styles from "./CreateBook.module.css";
+import { useState } from "react";
+import Button from '../../Form/Button'
+import Select from "../../Form/Select";
+
 
 function CreateBook() {
+
+  const [book, setBook] = useState({});
+
+  function handlerChangeBook(event) {
+    setBook({ ...book, [event.target.name]: event.target.value });
+    console.log(book);
+  }
+
+  function handlerChangeCategory(event) {
+    setBook({ ...book, category: event.target.options[event.target.selectedIndex].text })
+  }
+
+  function submit(event) {
+    event.preventDefault();
+    console.log(book);
+  }
+
+
+
   return (
-    <div className={styles.create_book_container}>
+    <form onSubmit={submit} className={styles.create_book_container}>
       <Input
-        type="text"
-        name="txt_nome"
-        id="txt_nome"
-        placeholder="Insira o nome do livro"
+        text='Nome do livro'
+        type='text'
+        name='txt_livro'
+        id='txt_livro'
+        placeholder='Digite o nome do livro'
+        handlerChange={handlerChangeBook}
       />
 
       <Input
-        type="text"
-        name="txt_descricao"
-        id="txt_descricao"
-        placeholder="Insira a descricao do livro"
+        text='Autor do livro'
+        type='text'
+        name='txt_autor'
+        id='txt_autor'
+        placeholder='Digite o nome do autor do livro'
+        handlerChange={handlerChangeBook}
       />
 
       <Input
-        type="text"
-        name="txt_autor"
-        id="txt_autor"
-        placeholder="Insira o autor do livro"
+        text='Descrição do livro'
+        type='text'
+        name='txt_descricao'
+        id='txt_descricao'
+        placeholder='Digite a descrição do livro'
+        handlerChange={handlerChangeBook}
+      />
+
+      <Select
+        name='slc_categoria'
+        id='slc_categoria'
+        text='Categoria do livro'
+        handlerChange={handlerChangeCategory}
       />
 
 
+      <Button
+        label='CADASTRAR LIVRO'
+      />
 
-
-
-
-    </div>
+    </form>
   );
 }
 
