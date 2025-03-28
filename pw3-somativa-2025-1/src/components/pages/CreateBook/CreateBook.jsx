dee9ecc53c211ba218e6cbb998ee6335868c7b25
LucasYohan <lucasyohanfmarinho@gmail.com>
2025-03-27 22:32:51 -0300

    Add requisições

--- pw3-somativa-2025-1/src/components/pages/CreateBook/CreateBook.jsx
+++ pw3-somativa-2025-1/src/components/pages/CreateBook/CreateBook.jsx
@@ -1,6 +1,6 @@
 import Input from "../../Form/Input";
 import styles from "./CreateBook.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from '../../Form/Button'
 import Select from "../../Form/Select";
 
@@ -8,6 +8,7 @@ import Select from "../../Form/Select";
 function CreateBook() {
 
   const [book, setBook] = useState({});
+  const [categories, setCategories] = useState([]);
 
   function handlerChangeBook(event) {
     setBook({ ...book, [event.target.name]: event.target.value });
@@ -15,7 +16,7 @@ function CreateBook() {
   }
 
   function handlerChangeCategory(event) {
-    setBook({ ...book, category: event.target.options[event.target.selectedIndex].text })
+    setBook({ ...book, cod_categoria: event.target.options[event.target.selectedIndex].text })
   }
 
   function submit(event) {
@@ -23,15 +24,31 @@ function CreateBook() {
     console.log(book);
   }
 
+  useEffect(() => {
+    fetch('http://localhost:5000/listagemCateorias', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*'
+      }
+    }).then((response) => 
+      response.json()
+    ).then((categorias)=>{
+        console.log('Teste: ' + categorias.data);
+    }).catch((error) => {
+      console.log('ERRO:' + error);
+    });
 
+  }, []);
 
   return (
     <form onSubmit={submit} className={styles.create_book_container}>
       <Input
         text='Nome do livro'
         type='text'
-        name='txt_livro'
-        id='txt_livro'
+        name='nome_livro'
+        id='nome_livro'
         placeholder='Digite o nome do livro'
         handlerChange={handlerChangeBook}
       />
@@ -39,8 +56,8 @@ function CreateBook() {
       <Input
         text='Autor do livro'
         type='text'
-        name='txt_autor'
-        id='txt_autor'
+        name='autor_livro'
+        id='autor_livro'
         placeholder='Digite o nome do autor do livro'
         handlerChange={handlerChangeBook}
       />
@@ -48,19 +65,18 @@ function CreateBook() {
       <Input
         text='Descrição do livro'
         type='text'
-        name='txt_descricao'
-        id='txt_descricao'
+        name='descricao_livro'
+        id='descricao_livro'
         placeholder='Digite a descrição do livro'
         handlerChange={handlerChangeBook}
       />
 
       <Select
-        name='slc_categoria'
-        id='slc_categoria'
+        name='cod_categoria'
+        id='cod_categoria'
         text='Categoria do livro'
         handlerChange={handlerChangeCategory}
       />
-
 
       <Button
         label='CADASTRAR LIVRO'
